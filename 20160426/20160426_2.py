7b5fc21c77a27472570c3f7c9c18383a166873a5
Ryan Shin <1982sdr@hanmail.net>
2016-05-03 17:11:36 +0900

    Starbucks Coding - 20160426_2 소스 추가. timeout 발생.

---
+++ 20160426/20160426_2.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def equal_sub(n, a):
+    for i in range(n):
+        if sum(a[:i]) == sum(a[i+1:]):
+            return i
+    return -1
+
+t = int(raw_input())
+for _ in range(t):
+    n = int(raw_input())
+    s = raw_input()
+    a = [int(x) for x in s.split()]
+    if equal_sub(n,a) >= 0:
+        print "YES"
+    else:
+        print "NO"

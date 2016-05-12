98886dfdcf146d980df01a2f062ff5b8b31a420a
Ryan Shin <1982sdr@hanmail.net>
2016-05-12 13:37:46 +0900

    Starbucks Coding - 20160510_1 추가함. 일부 testcase timeout(10초 이상) 발생.

---
+++ 20160510/20160510_1.py
@@ -0,0 +1,18 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def sum_primes(n):
+    primes = range(n+1)	# [0,1,2,...,n]인 리스트 생성
+    primes[1] = 0	# 1은 소수가 아니라 0 대임
+    import math		# 에라스토스의 채를 이용하여 소수가 아닌 값에 0 대입
+    for base in range(2,int(math.sqrt(n))+1):
+        s = 2
+        while base * s <= n:
+            primes[base * s] = 0
+            s += 1
+    return sum(primes)
+
+t = int(raw_input())
+for _ in range(t):
+    n = int(raw_input())
+    print sum_primes(n)
---
+++ 20160510/README.md
@@ -0,0 +1,11 @@
+20160510_1
+5월 10일 첫번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler010
+
+20160510_2
+5월 10일 두번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler011
+
+20160510_3
+5월 10일 세번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler012

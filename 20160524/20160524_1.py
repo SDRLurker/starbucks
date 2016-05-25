2636cd9b97b517506a53e8498d55b23a15dc5ec7
Ryan Shin <1982sdr@hanmail.net>
2016-05-25 19:04:20 +0900

    Starbucks Coding - 20160524_1 수정함. 모든 testcase 통과.

--- 20160524/20160524_1.py
+++ 20160524/20160524_1.py
@@ -1,25 +1,30 @@
 #!/use/bin/python
 # -*- coding:utf-8 -*-
 class Solution(object):
+    def isVowel(self, c):
+        vowels = list('aeiouAEIOU')
+        return c in vowels
+
     def reverseVowels(self, s):
         """
         :type s: str
         :rtype: str
         """
-        vowel_idx = {}
-        for i,c in enumerate(s):
-            if c in 'aeiouAEIOU':
-                vowel_idx[i] = c
-        r = ''
-        back_idx = len(vowel_idx) - 1
-        v_idx = sorted(vowel_idx.items())
-        for i,c in enumerate(s):
-            if c in 'aeiouAEIOU':
-                r += str(v_idx[back_idx][1])
-                back_idx -= 1
-            else:
-                r += c
-        return r
+        str_l = list(s) 
+	s_idx = 0
+	e_idx = len(s)-1
+	while s_idx <= e_idx:
+	    while s_idx < len(s)-1 and self.isVowel(str_l[s_idx]) == False:
+	        s_idx += 1
+	    while e_idx > 0 and self.isVowel(str_l[e_idx]) == False:
+	        e_idx -= 1
+	    if s_idx > e_idx:
+	    	break
+            str_l[s_idx], str_l[e_idx] = str_l[e_idx], str_l[s_idx]
+	    s_idx += 1
+	    e_idx -= 1
+
+        return "".join(str_l)
 
 s = Solution()
 t = int(raw_input())

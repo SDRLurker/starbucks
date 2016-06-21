bfcaa75b6f2b752aaf6739cbeb2dafce5b36ff22
Ryan Shin <1982sdr@hanmail.net>
2016-06-21 23:53:25 +0900

    Starbucks Coding - 20160621_2 추가함. 모든 testcase 실패.

---
+++ 20160621/20160621_2.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for y in xrange(n):
+            for x in xrange(n):
+                tmp = matrix[y][x]
+                matrix[y][x] = matrix[x][y]
+                matrix[x][y] = tmp
+        for y in xrange(n):
+            for x in xrange(n//2):
+                tmp = matrix[y][x]
+                matrix[y][x] = matrix[y][n-x-1]
+                matrix[y][n-x-1] = tmp
+
+s = Solution()
+print "%20s %20s %20s" % ("input", "Expected", "Result")
+input = [[1,2],[3,4]]
+result = [[1,2],[3,4]]
+s.rotate(result)
+print "%20s %20s %20s" % (str(input), "[[3,1],[4,2]]", str(result))
---
+++ 20160621/README.md
@@ -0,0 +1,11 @@
+﻿20160621_1
+6월 21일 첫번째 문제입니다.
+https://leetcode.com/problems/pascals-triangle-ii/
+
+20160621_2
+6월 21일 두번째 문제입니다.
+https://leetcode.com/problems/rotate-image/
+
+20160621_3
+6월 21일 세번째 문제입니다.
+https://leetcode.com/problems/palindrome-pairs/

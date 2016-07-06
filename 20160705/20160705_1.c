f5a3dbc07c917809fee0d65609627fdf15e58a56
Ryan Shin <1982sdr@hanmail.net>
2016-07-07 07:09:14 +0900

    Starbucks Coding - 20160705_1 추가함. 일부 testcase sig=11 발생.

---
+++ 20160705/20160705_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int getSum(int a, int b) {
+    if( a & b == 0)
+        return a ^ b;
+    return getSum(a ^ b, (a & b) << 1);
+}
+
+int main()
+{
+	printf("%12s %12s %12s %12s\n", "a", "b", "Output", "Expected");
+	printf("%12d %12d %12d %12d\n", 1, 2, getSum(1,2), 3);
+	printf("%12d %12d %12d %12d\n", -1, 1, getSum(-1,1), 0);
+	printf("%12d %12d %12d %12d\n", 20, 30, getSum(20,30), 50);
+	return 0;
+}

f489002c404dc95382857b56e34d388a42f13dc5
Ryan Shin <1982sdr@hanmail.net>
2016-07-13 00:59:08 +0900

    Starbucks Coding - 20160712_2 수정함. 모든 testcase 통과.

--- 20160712/20160712_2.py
+++ 20160712/20160712_2.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def __init__(self):
@@ -11,18 +11,17 @@ class Solution(object):
         self.r = []
             
     def getLeft(self, left):
-        # when left is None, we can't get left.left by runtime error.
-        self.getLeft(left.left)
-        self.getLeft(left.right)
         if left:
+            self.getLeft(left.left)
+            self.getLeft(left.right)
             self.l.append(left.val)
         else:
             self.l.append("")
             
     def getRight(self, right):
-        self.getRight(right.right)
-        self.getRight(right.left)
         if right:
+            self.getRight(right.right)
+            self.getRight(right.left)
             self.r.append(right.val)
         else:
             self.r.append("")
@@ -41,3 +40,24 @@ class Solution(object):
         self.getLeft(root.left)
         self.getRight(root.right)
         return self.l == self.r
+
+def makeTree(vals):
+    nodes = []
+    for i in vals:
+        node = TreeNode(i)
+        nodes.append(node)
+    i = 0
+    while i < len(vals):
+        nodes[i].left = nodes[(i+1) * 2 - 1] if (i+1) * 2 - 1 < len(vals) else None
+        nodes[i].right = nodes[(i+1) * 2] if (i+1) * 2 < len(vals) else None
+        i += 1
+    return nodes[0]
+
+s = Solution()
+vals = [1,2,2,3,4,4,3]
+root = makeTree(vals)
+print "%35s %10s %10s" % ("input", "Expected", "Result")
+print "%35s %10s %10s" % (vals, "True", s.isSymmetric(root)) 
+vals = [1,2,2,None,3,None,3]
+root = makeTree(vals)
+print "%35s %10s %10s" % (vals, "False", s.isSymmetric(root)) 

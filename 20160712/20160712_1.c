0c50fc1a47e1fcc629a39e290a317b000f5461a7
Ryan Shin <1982sdr@hanmail.net>
2016-07-12 23:44:56 +0900

    Starbucks Coding - 20160712_1 c 소스 추가함. 일부 testcase trim 처리를 안 했으므로 실패.

---
+++ 20160712/20160712_1.c
@@ -0,0 +1,26 @@
+#include<string.h>
+#include<stdio.h>
+
+int lengthOfLastWord(char* s) {
+    int i = strlen(s) - 1;
+    int c = 0;
+    for(;i>=0;i--){
+        if(s[i] == ' ')
+            break;
+        else
+            c++;
+    }
+    return c;
+}
+
+int main()
+{
+    printf("%20s %8s %8s\n", "Input", "Output", "Expected");
+    printf("%20s %8d %8d\n", "\"\"", lengthOfLastWord(""), 0);
+    printf("%20s %8d %8d\n", "\" \"", lengthOfLastWord(" "), 0);
+    // the output is 0, the solution is 1. That's why we need trim first.
+    printf("%20s %8d %8d\n", "\"a \"", lengthOfLastWord("a "), 1);
+    printf("%20s %8d %8d\n", "Hello World", 
+        lengthOfLastWord("Hello World"), 5);
+    return 0;
+}
--- 20160712/20160712_1.py
+++ 20160712/20160712_1.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+	# when len == 0 (s = '' or s = ' '), IndexError occurs
 	ws = s.split()
 	return len(ws[-1]) if len(ws) > 0 else 0
 
@@ -11,4 +12,5 @@ s = Solution()
 print "%20s %8s %8s" % ("Input", "Output", "Expected")
 print "%20s %8d %8d" % ('""', s.lengthOfLastWord(""), 0)
 print "%20s %8d %8d" % ('" "', s.lengthOfLastWord(" "), 0)
+print "%20s %8d %8d" % ('"a "', s.lengthOfLastWord("a "), 1)
 print "%20s %8d %8d" % ("Hello World", s.lengthOfLastWord("Hello World"), 5)

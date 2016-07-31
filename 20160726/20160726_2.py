ca2688ef659b723f67366d4de43b78af6d84209f
Ryan Shin <1982sdr@hanmail.net>
2016-08-01 01:24:24 +0900

    Starbucks Coding - 20160726_2 수정함. 일부 testcase timeout 발생.

--- 20160726/20160726_2.py
+++ 20160726/20160726_2.py
@@ -5,49 +5,40 @@ class Solution(object):
         :type amount: int
         :rtype: int
         """
-        self.coins = coins
-        self.coins.sort(reverse=True)
-        self.amount = amount
-        self.min_num = -1
-	self.min_k = amount+1
-        self.backtrack([0 for x in xrange(amount)], 0)
-        return self.min_num
-        
-    def backtrack(self, used_coins, k):
-        if self.is_a_solution(used_coins, k):
-            num = sum([1 for c in used_coins[0:k] if c > 0])
-            if self.min_num == -1 or (num > 0 and self.min_num > num) :
-                self.min_num = num
-		self.min_k = k
-        else:
-            k = k + 1
-            if k >= self.min_k:
-                return
-            candidates = self.get_candidates(used_coins, k)
-            for c in candidates:
-                used_coins[k-1] = c
-                self.backtrack(used_coins, k)
-                
-    def is_a_solution(self, used_coins, k):
-        if sum(used_coins[0:k]) == self.amount:
-            return True
-        return False
-        
-    def get_candidates(self, used_coins, k):
-        c=[]
-        for coin in self.coins:
-            if self.amount - sum(used_coins[0:k-1]) - coin >= 0:
-                c.append(coin)
-        return c
+        used_coins = [ amount+1 for _ in xrange(amount) ]
+	coins.sort(reverse = True)
+        for coin in coins:
+            i = amount - 1
+            while i >= 0:
+                if used_coins[i] <= amount:  
+                    cnt = used_coins[i]
+                    self.check_used_coins(coin, amount, used_coins, i, cnt)
+                i -= 1
+	    self.check_used_coins(coin, amount, used_coins, i, 0)
+            first = False
+        used_coins = [0] + used_coins
+        last = used_coins[-1]
+	return last if last <= amount else -1
 
+    def check_used_coins(self, coin, amount, used_coins, i, cnt):
+        while i < amount:
+            i += coin
+            if i >= amount:
+                break
+            cnt += 1
+            if cnt < used_coins[i]:
+                used_coins[i] = cnt
+                    
 def solve_string(s, coins, amount, expected):
 	return "%20s %10d %10d %10d" % (coins, amount, expected, s.coinChange(coins, amount))
 
 s = Solution()
 print "%20s %10s %10s %10s" % ("coins", "amount", "Expected", "Result")
 print solve_string(s, [1], 0, 0) 
+print solve_string(s, [1], 1, 1) 
 print solve_string(s, [1,2,5], 11, 3) 
 print solve_string(s, [2], 3, -1) 
 print solve_string(s, [1,4,5], 8, 2) 
 print solve_string(s, [2,5], 11, 4) 
-#print solve_string(s, [1,2,5], 100, 20) 
+print solve_string(s, [1,2,5], 100, 20) 
+print solve_string(s, [3,7,405,436], 8839, 25)

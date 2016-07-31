8cbcc59fb2f48a21db2a3904d36bf66eaa0b8e31
Ryan Shin <1982sdr@hanmail.net>
2016-08-01 01:45:38 +0900

    Starbucks Coding - 20160726_2 추가함. 모든 testcase 통과.

--- 20160726/20160726_2.py
+++ 20160726/20160726_2.py
@@ -6,29 +6,16 @@ class Solution(object):
         :rtype: int
         """
         used_coins = [ amount+1 for _ in xrange(amount) ]
-	coins.sort(reverse = True)
-        for coin in coins:
-            i = amount - 1
-            while i >= 0:
-                if used_coins[i] <= amount:  
-                    cnt = used_coins[i]
-                    self.check_used_coins(coin, amount, used_coins, i, cnt)
-                i -= 1
-	    self.check_used_coins(coin, amount, used_coins, i, 0)
-            first = False
         used_coins = [0] + used_coins
+        coins.sort(reverse = True)
+        for i in xrange(amount+1): 
+            for coin in coins:
+                if i < coin:
+                    continue
+                used_coins[i] = min(used_coins[i], used_coins[i-coin] + 1)
         last = used_coins[-1]
 	return last if last <= amount else -1
 
-    def check_used_coins(self, coin, amount, used_coins, i, cnt):
-        while i < amount:
-            i += coin
-            if i >= amount:
-                break
-            cnt += 1
-            if cnt < used_coins[i]:
-                used_coins[i] = cnt
-                    
 def solve_string(s, coins, amount, expected):
 	return "%20s %10d %10d %10d" % (coins, amount, expected, s.coinChange(coins, amount))
 
@@ -42,3 +29,5 @@ print solve_string(s, [1,4,5], 8, 2)
 print solve_string(s, [2,5], 11, 4) 
 print solve_string(s, [1,2,5], 100, 20) 
 print solve_string(s, [3,7,405,436], 8839, 25)
+print solve_string(s, [370,417,408,156,143,434,168,83,177,280,117], 9953, 24)
+print solve_string(s, [84,457,478,309,350,349,422,469,100,432,188], 6993, 15)

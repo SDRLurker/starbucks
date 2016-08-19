adcadc89afe45c1bd7b623da19737a8522234de5
Ryan Shin <1982sdr@hanmail.net>
2016-08-19 18:35:29 +0900

    Starbucks Coding - 20160816_2 추가함. 일부 testcase 실패.

---
+++ 20160816/20160816_2.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def compare(self, a, b):
+        return 0 if a-b == 0 else (a-b) // abs(a-b)
+        
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        ver1 = version1.split('.')
+        ver2 = version2.split('.')
+        for i, lvl in enumerate(ver1):
+            if i >= len(ver2):
+                break
+            a = self.compare(int(lvl), int(ver2[i]))
+            if a != 0:
+                return a
+        return self.compare(len(ver1), len(ver2))
+
+def solve_string(s, ver1, ver2, expected):
+	return "%10s %10s %10d %10d" % (ver1, ver2, expected, s.compareVersion(ver1, ver2))
+
+s = Solution()
+print("%10s %10s %10s %10s" % ("version1", "version2", "Expected", "Result"))
+print(solve_string(s, "1.0", "1", 0))
+print(solve_string(s, "1", "1.1", -1))
+print(solve_string(s, "1.1", "1.01.0", 0))
+print(solve_string(s, "1.0.1", "1", 1))
---
+++ 20160816/README.md
@@ -0,0 +1,11 @@
+﻿20160816_1
+8월 16일 첫번째 문제입니다.
+https://leetcode.com/problems/ransom-note/
+
+20160816_2
+8월 16일 두번째 문제입니다.
+https://leetcode.com/problems/compare-version-numbers/
+
+20160816_3
+8월 16일 세번째 문제입니다.
+https://leetcode.com/problems/shuffle-an-array/

3c10baf80dd0e2aa351c1ffd78ce5ed83ef6033f
Ryan Shin <1982sdr@hanmail.net>
2016-09-07 00:27:41 +0900

    Starbucks Coding - 20160906_1 수정함. 모든 testcase 성공. (xor연산으로 해결.)

--- 20160906/20160906_1.py
+++ 20160906/20160906_1.py
@@ -5,16 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        letters = {}
-        for c in s:
-            letters[c] = letters.get(c,0) + 1
-        for c in t:
-            if c not in letters:
-                return c
-            else: 
-                letters[c] = letters.get(c,0) - 1
-        # refers to http://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary
-        return list(letters.keys())[list(letters.values()).index(1)] if 1 in letters.values() else list(letters.keys())[list(letters.values()).index(-1)]
+        letter = 0
+        for c in s+t:
+            letter = letter ^ ord(c)
+        return chr(letter)
 
 def solve_string(solution, s, t, expected):
         return "%10s %10s %8s %8s" % (s, t, expected, solution.findTheDifference(s,t))

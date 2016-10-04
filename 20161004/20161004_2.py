7eadbb5cea02b079167308cb9df53e3189904204
Ryan Shin <1982sdr@hanmail.net>
2016-10-04 23:52:21 +0900

    Starbucks Coding - 20161004_2 추가함. 일부 testcase 실패.

---
+++ 20161004/20161004_2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        s = ""
+        cs = "0123456789abcdef"
+        for _ in range(8):
+            s = cs[num&0xf] + s
+            num >>= 4
+        return s.lstrip("0")
+
+def solve_string(s, num, expected):
+        return "%10d %10s %10s" % (num, expected, s.toHex(num))
+
+s = Solution()
+print("%10s %10s %10s" % ("num", "Expected", "Result"))
+print(solve_string(s, 26, "1a"))
+print(solve_string(s, -1, "ffffffff"))
+print(solve_string(s, 0, "0"))

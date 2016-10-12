5de73ec4a41b24a9240f6c7bf19e20ff424adadf
Ryan Shin <1982sdr@hanmail.net>
2016-10-12 19:02:52 +0900

    Starbucks Coding - 20161011_1 추가함.  일부 testcast 실패.

---
+++ 20161011/20161011_1.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        l1 = len(num1)
+        l2 = len(num2)
+        answer = [0 for x in range(max(l1, l2) + 1)]
+        for i in range(len(answer)-1):
+            d1 = ord(num1[l1-1-i]) - ord('0') if i < len(num1) else 0
+            d2 = ord(num2[l2-1-i]) - ord('0') if i < len(num2) else 0
+            answer[i] += ((d1+d2) % 10)
+            answer[i+1] = ((d1+d2) // 10)
+        result = "".join([str(c) for c in answer[::-1]]).lstrip("0")
+        return "0" if len(result) == 0 else result
+
+def solve_string(s, n1, n2, expected):
+	return "%10s %10s %20s %20s" % (n1, n2, expected, s.addStrings(n1,n2))
+
+s = Solution()
+print("%10s %10s %20s %20s" % ("num1", "num2", "Expected", "Result"))
+print(solve_string(s, "0", "0", "0"))
+print(solve_string(s, "584", "18", "602"))
---
+++ 20161011/README.md
@@ -0,0 +1,8 @@
+﻿20161011_1
+10월 11일 첫번째 문제입니다.
+https://leetcode.com/problems/add-strings/
+
+20161011_2
+10월 11일 두번째 문제입니다.
+https://leetcode.com/problems/partition-equal-subset-sum/
+

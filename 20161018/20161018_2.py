168f37f5010a43f8feb82d5cf57ca078efab22e9
Ryan Shin <1982sdr@hanmail.net>
2016-10-21 15:04:43 +0900

    Starbucks Coding - 20161018_2 수정함. 모든 testcase 성공.

--- 20161018/20161018_2.py
+++ 20161018/20161018_2.py
@@ -4,22 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        numbers = ("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        evens = ("zero", "two", "four", "six", "eight")
+        odds = ("one", "three", "five", "seven", "nine")
         c_cnt = [0 for x in range(26)]
+        d_cnt = [0 for x in range(10)]
         for c in s:
             c_cnt[ord(c)-ord('a')] += 1
-        num_str = ""
-        for si, n in enumerate(numbers):
-            stay = True
-            while stay:
-                for c in n:
-                    if c_cnt[ord(c)-ord('a')] == 0:
-                        stay = False
-                        break
-                if stay:
+        for i, tup in enumerate((evens, odds)):
+            for si, n in enumerate(tup):
+                stay = True
+                while stay:
                     for c in n:
-                        c_cnt[ord(c)-ord('a')] -= 1
-                    num_str += str(si)
+                        if c_cnt[ord(c)-ord('a')] == 0:
+                            stay = False
+                            break
+                    if stay:
+                        for c in n:
+                            c_cnt[ord(c)-ord('a')] -= 1
+                        d_cnt[si*2+i] += 1
+        num_str = ""
+        for i, d in enumerate(d_cnt):
+            num_str += (d * str(i))
         return num_str
 
 def solve_string(solution, s, expected):
--- 20161018/README.md
+++ 20161018/README.md
@@ -1,10 +1,15 @@
-﻿20161018_1
+20161018_1
 10월 18일 첫번째 문제입니다.
 https://leetcode.com/problems/battleships-in-a-board/
 
 20161018_2
 10월 18일 두번째 문제입니다.
 https://leetcode.com/problems/reconstruct-original-digits-from-english/
+
+####Discussion
+
+중복되지 않는 유일한 숫자에 대해서 먼저 처리한 뒤에 중복되는 숫자를 처리해야 정확하게 문제를 풀 수 있습니다.
+그렇지 않으면 "twonine" => "29"가 나와야 하는데 "1"이 나오는 문제가 발생합니다.
 
 20161018_3
 10월 18일 세번째 문제입니다.

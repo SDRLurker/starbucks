d11730cb45b62a4d6b4a8c18c9b10d07b0545f5d
Ryan Shin <1982sdr@hanmail.net>
2017-03-27 01:33:49 +0900

    Starbucks Coding - 20170323_2 추가함. 모든 testcase 통과.

---
+++ 20170323/20170323_2.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def flip(self, s):
+        stat = s[0]
+        cnt = 0
+        for i, c in enumerate(s):
+            if stat != c:
+                cnt += 1
+                stat = c
+            if i == len(s) - 1 and c == '-':
+                cnt += 1
+        return cnt
+
+def solve_string(solution, s, expected):
+    return "%10s %10d %10d" % (s, expected, solution.flip(s))
+
+s = Solution()
+print("%10s %10s %10s" % ("s", "Expected", "Result"))
+print(solve_string(s, "-", 1))
+print(solve_string(s, "-+", 1))
+print(solve_string(s, "+-", 2))
+print(solve_string(s, "+++", 0))
+print(solve_string(s, "--+-", 3))

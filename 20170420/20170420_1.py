a13afb4a42777b184bab05b77a9803ad84a7aa66
Ryan Shin <1982sdr@hanmail.net>
2017-04-23 08:25:29 +0900

    Starbucks Coding - 20170420_1 추가함. 일부 testcase 실패.

---
+++ 20170420/20170420_1.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        a = 0
+        l = 0
+        for c in s:
+            if c == 'A':
+                a += 1
+                l = 0
+            elif c == 'L':
+                l += 1
+                a = 0
+            else:
+                a = 0
+                l = 0
+            if a > 1 or l > 2:
+                return False
+        return True
+
+def solve_string(solution, s, expected):
+    return "%40s %10s %10s" % (s, expected, solution.checkRecord(s))
+
+s = Solution()
+print("%40s %10s %10s" % ("s", "Expected", "Result"))
+print(solve_string(s, "PPALLP", True))
+print(solve_string(s, "PPALLL", False))
+print(solve_string(s, "ALLAPPL", False))
---
+++ 20170420/README.md
@@ -0,0 +1,11 @@
+﻿20170420_1
+4월 20일 첫번째 문제입니다.
+https://leetcode.com/submissions/detail/100848314/
+
+20170420_2
+4월 20일 두번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler016
+
+20170420_3
+4월 20일 세번째 문제입니다.
+https://leetcode.com/problems/optimal-division/#/description

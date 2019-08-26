11a1a5d66f9e18a94a1b177be5dc8a8a8cee7be3
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2019-08-26 13:44:33 +0900

    Starbucks Coding - 20161025_2 수정함. 모든 testcase 성공.

--- 20161025/20161025_2.py
+++ 20161025/20161025_2.py
@@ -1,29 +1,33 @@
 class Solution(object):
+    def countLetter(self, s):
+        cnt_dic = {}
+        for c in s:
+            cnt_dic[c] = cnt_dic.get(c, 0) + 1
+        return cnt_dic
+
     def findAnagrams(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: List[int]
         """
-        indice = []
-        if not s or len(s) < len(p):
-            return []
-        p_cnt = [0 for x in range(26)]
-        s_cnt = [0 for x in range(26)]
-        for c in p:
-            p_cnt[ord(c)-ord('a')] += 1
-        for c in s[:len(p)]:
-            s_cnt[ord(c)-ord('a')] += 1
-        si = len(p)
-        while si < len(s):
-            if s_cnt == p_cnt:
-                indice.append(si-len(p))
-            s_cnt[ord(s[si-len(p)])-ord('a')] -= 1
-            s_cnt[ord(s[si])-ord('a')] += 1
-            si += 1
-        if s_cnt == p_cnt:
-            indice.append(si-len(p))
-        return indice
+        answer = []
+        p_dic = self.countLetter(p)
+        s_dic = self.countLetter(s[:len(p)])
+        for i in range(len(s)-len(p)):
+            if p_dic == s_dic:
+                answer.append(i)
+            if i + len(p) < len(s):
+                s_dic[s[i+len(p)]] = s_dic.get(s[i+len(p)], 0) + 1
+            if s[i] in s_dic:
+                s_dic[s[i]] = s_dic[s[i]] - 1
+                if s_dic[s[i]] == 0:
+                    s_dic.pop(s[i])
+
+
+        if p_dic == s_dic:
+            answer.append(i+1)
+        return answer
 
 def solve_string(solution, s, p, expected):
 	return "%10s %10s %20s %20s" % (s, p, expected, solution.findAnagrams(s,p))

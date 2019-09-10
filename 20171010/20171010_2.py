4ab7b65650edfb7f7597b83d28127fd3dfc9257d
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2019-09-10 14:17:51 +0900

    Starbucks Coding - 20171010_2 수정함. 모든 testcase 통과.

--- 20171010/20171010_2.py
+++ 20171010/20171010_2.py
@@ -1,31 +1,25 @@
 class Solution:
     def numDecodings(self, s):
-        self.cnt = 0
+        for i in range(1, 9+1):
+            ii = s.find("%02d" % i)
+            if ii == 0 or (ii > 0 and s[ii-1] in '03456789') or s.startswith("0"):
+                return 0
+        #print(s)
         a = [0 for _ in range(len(s)+1)]
-        self.backtrack(a, s, 0)
-        return self.cnt
-    
-    def backtrack(self, a, s, k):
-        if self.is_solution(a, s, k):
-            self.cnt += 1
-        k = k + 1
-        c = self.candidate(a, s, k)
-        for i in range(len(c)):
-            a[k] = c[i]
-            self.backtrack(a, s, k)
-    
-    def candidate(self, a, s, k):
-        c = []
-        p = a[k-1]
-        if 0 <= p < len(s) and s[p] != '0':
-            c.append(p+1)
-        if 0 <= p < len(s)-1 and 10 <= int(s[p:p+2]) <= 26:
-            c.append(p+2)
-        return c
-            
-    def is_solution(self, a, s, k):
-        if a[k] == len(s):
-            return True
+        for i in range(len(s)):
+            #print(i, a)
+            if s[i] != "0":
+                a[i+1] = 1 if a[i] == 0 else a[i]
+            if i > 0 and 11 <= int(s[i-1:i+1]) <= 26 and int(s[i-1:i+1]) != 20:
+                if i > 1 and 11 <= int(s[i-2:i]) <= 26:
+                    a[i+1] = a[i] + a[i-1]
+                else:
+                    a[i+1] = a[i] * 2
+            elif i > 0 and int(s[i-1:i+1]) in (10,20):
+                a[i+1] = a[i-1] if a[i] > 1 else a[i]
+            #print(i, a)
+        return a[len(s)]
+
 
 import unittest
 class test_solution(unittest.TestCase):
@@ -34,7 +28,15 @@ class test_solution(unittest.TestCase):
         self.assertEqual(s.numDecodings("12"), 2)
         self.assertEqual(s.numDecodings("226"), 3)
         self.assertEqual(s.numDecodings("0"), 0)
+        self.assertEqual(s.numDecodings("01"), 0)
+        self.assertEqual(s.numDecodings("012"), 0)
         self.assertEqual(s.numDecodings("10"), 1)
+        self.assertEqual(s.numDecodings("110"), 1)
+        self.assertEqual(s.numDecodings("1110"), 2)
+        self.assertEqual(s.numDecodings("301"), 0)
+        self.assertEqual(s.numDecodings("12120"), 3)
+        self.assertEqual(s.numDecodings("04759"), 0)
+        self.assertEqual(s.numDecodings("509"), 0)
         self.assertEqual(s.numDecodings("4757562545844617494555774581341211511296816786586787755257741178599337186486723247528324612117156948"), 589824)
 
 if __name__ == "__main__":

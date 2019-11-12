16bcfb7d62c95f67ce7851ca7b3e0752291639f2
Ryan Shin <1982sdr@hanmail.net>
2019-11-12 23:47:40 +0900

    Starbucks Coding - 20150217_1 추가함. 모든 testcase 성공.

---
+++ 20150217/20150217_1.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def _solve(self, node, val):
+        if not node:
+            return
+        if node and not node.left and not node.right:
+            self.result += (val*10 + node.val)
+            return        
+        if node.left:
+            self._solve(node.left, val*10 + node.val)
+        if node.right:
+            self._solve(node.right, val*10 + node.val)
+            
+            
+    def sumNumbers(self, root):
+        self.result = 0
+        self._solve(root, 0)
+        return self.result
+
+def makeTree(vals):
+    nodes = []
+    for i in vals:
+        node = TreeNode(i)
+        nodes.append(node)
+    i = 0
+    while i < len(vals):
+        nodes[i].left = nodes[(i+1) * 2 - 1] if (i+1) * 2 - 1 < len(vals) and vals[(i+1) * 2 - 1] is not None else None
+        nodes[i].right = nodes[(i+1) * 2] if (i+1) * 2 < len(vals) and vals[(i*1) * 2] is not None else None
+        i += 1
+    return nodes[0]
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        vals = [1,2,3]
+        trees = makeTree(vals)
+        s = Solution()
+        self.assertEqual(s.sumNumbers(trees), 25)
+
+        vals = [4,9,0,5,1]
+        trees = makeTree(vals)
+        self.assertEqual(s.sumNumbers(trees), 1026)
+
+        self.assertEqual(s.sumNumbers(None), 0)
+
+        #vals = [2,7,4,None,4,0,None,None,2,None,None,None,1]
+        #trees = makeTree(vals)
+        #self.assertEqual(s.sumNumbers(trees), 27661)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 20150217/README.md
@@ -0,0 +1,5 @@
+﻿20150217_1
+
+신년회 문제
+
+https://leetcode.com/problems/sum-root-to-leaf-numbers

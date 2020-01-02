458acfb76195f0d68009ae1fdf72d23c613f7f9f
Ryan Shin <1982sdr@hanmail.net>
2020-01-02 22:18:35 +0900

    Starbucks Coding 20160308_1 추가함. 모든 testcase 성공.

---
+++ 20160308/20160308_1.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Complete the squares function below.
+def angryProfessor(k, a):
+    return "YES" if sum(1 for b in a if b <= 0) < k else "NO"
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(angryProfessor(3, [-1, -3, 4, 2]), "YES")
+        self.assertEqual(angryProfessor(2, [0, -1, 2, 1]), "NO")
+
+if __name__ == "__main__":
+    unittest.main()
+

bfb1d1e65d9e6f5c30e09764537b80eb78452b75
Ryan Shin <1982sdr@hanmail.net>
2020-01-08 23:50:08 +0900

    Starbucks Coding - 20160329_1 추가함. 모든 testcase 성공.

---
+++ 20160329/20160329_1.py
@@ -0,0 +1,27 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Complete the caesarCipher function below.
+def caesarCipher(s, k):
+    es = ""
+    for c in s:
+        n = ord(c)
+        if c.isupper():
+            n = (ord(c)-ord('A')+k)%26+ord('A')
+        elif c.islower():
+            n = (ord(c)-ord('a')+k)%26+ord('a')
+        es += chr(n)
+    return es
+
+
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(caesarCipher("middle-Outz",2), "okffng-Qwvb")
+        i = "Always-Look-on-the-Bright-Side-of-Life"
+        o = "Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"
+        self.assertEqual(caesarCipher(i,5), o)
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ 20160329/README.md
@@ -0,0 +1,11 @@
+20160329_1
+3월 29일 첫번째 문제입니다.
+https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+
+20160329_2
+3월 29일 두번째 문제입니다.
+https://www.hackerrank.com/challenges/manasa-and-stones/problem
+
+20160329_3
+3월 29일 세번째 문제입니다.
+https://www.hackerrank.com/challenges/encryption/problem

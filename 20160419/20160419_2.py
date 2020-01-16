8f4ada04701b753bdd14b25304f965b9485c8b1f
Ryan Shin <1982sdr@hanmail.net>
2020-01-16 14:42:35 +0000

    Starbucks Coding - 20160419_2 추가함. 모든 testcase 성공.

---
+++ 20160419/20160419_2.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def max_product(num, k):
+    m = -1
+    for i in range(len(num)-k):
+        p = 1
+        for kk in range(k):
+            p *= int(num[i+kk])
+        m = max(m, p)
+    return m
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(max_product("3675356291", 5), 3150)
+        self.assertEqual(max_product("2709360626", 5), 0)
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ 20160419/README.md
@@ -0,0 +1,11 @@
+20160419_1
+4월 19일 첫번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem
+
+20160419_2
+4월 19일 두번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler008/problem
+
+20160419_3
+4월 19일 세번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler009/problem

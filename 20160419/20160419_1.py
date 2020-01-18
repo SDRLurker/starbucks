6ee60b0a94f62013f14c3d4d36532f06bfdf53ea
Ryan Shin <1982sdr@hanmail.net>
2020-01-18 11:14:59 +0900

    Starbucks Coding - 20160419_1 추가함. 모든 testcase 성공.

---
+++ 20160419/20160419_1.py
@@ -0,0 +1,40 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def is_prime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+def generator_prime():
+    p = 2
+    while True:
+        if is_prime(p):
+            yield p
+        p = 3 if p == 2 else p + 2
+    
+prime_dic = {}
+def prime_seq(n):
+    if n in prime_dic:
+        return prime_dic[n]
+    i = 0
+    pp = 0
+    for p in generator_prime():
+        i += 1
+        prime_dic[i] = p
+        if i == n:
+            pp = p
+            break
+    return p
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(prime_seq(10000), 104729)
+        self.assertEqual(prime_seq(3), 5)
+        self.assertEqual(prime_seq(6), 13)
+
+
+if __name__ == "__main__":
+    unittest.main()
+

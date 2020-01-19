be92ca511625e01d049d05375842542240755c83
Ryan Shin <1982sdr@hanmail.net>
2020-01-19 20:31:18 +0900

    Starbucks Coding - 20160329_3 추가함. 모든 testcase 성공.

---
+++ 20160329/20160329_3.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Complete the caesarCipher function below.
+import math
+def encryption(s):
+    col = int(math.ceil(len(s)**0.5))
+    arr = []
+    for i in range(col):
+        arr.append(s[i:len(s):col])
+    return " ".join(arr)
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(encryption("haveaniceday"), "hae and via ecy")
+        self.assertEqual(encryption("feedthedog"), "fto ehg ee dd")
+        self.assertEqual(encryption("chillout"), "clu hlt io")
+
+if __name__ == "__main__":
+    unittest.main()
+

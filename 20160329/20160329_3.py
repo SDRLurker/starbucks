679514ecae077c16f59dc79a6a48b706329ce1c5
Ryan Shin <1982sdr@hanmail.net>
2020-01-20 23:39:35 +0900

    Starbucks Coding - 20160329_3 수정함. 모든 testcase 성공.

--- 20160329/20160329_3.py
+++ 20160329/20160329_3.py
@@ -3,11 +3,8 @@
 # Complete the caesarCipher function below.
 import math
 def encryption(s):
-    col = int(math.ceil(len(s)**0.5))
-    arr = []
-    for i in range(col):
-        arr.append(s[i:len(s):col])
-    return " ".join(arr)
+    l = int(math.ceil(len(s)**0.5))
+    return " ".join([s[i::l] for i in range(l)])
 
 import unittest
 class test_solution(unittest.TestCase):
@@ -15,7 +12,7 @@ class test_solution(unittest.TestCase):
         self.assertEqual(encryption("haveaniceday"), "hae and via ecy")
         self.assertEqual(encryption("feedthedog"), "fto ehg ee dd")
         self.assertEqual(encryption("chillout"), "clu hlt io")
+        self.assertEqual(encryption("iffactsdontfittotheorychangethefacts"), "isieae fdtonf fotrga anoyec cttctt tfhhhs")
 
 if __name__ == "__main__":
     unittest.main()
-

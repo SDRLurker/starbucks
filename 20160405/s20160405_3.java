da0794a09ef437a09f83fb84a91174566ab405c6
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2020-01-23 23:57:46 +0900

    Starbucks Coding - s20160405_3.java 추가함. 모든 testcase 성공.

---
+++ 20160405/s20160405_3.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class s20160405_3 {
+    private boolean isAnagram(String a, String b) {
+        byte a_num[] = new byte[26];
+        byte b_num[] = new byte[26];
+        if(a.length()!=b.length())
+            return false;
+        for(int i=0;i<a.length();i++) {
+            a_num[a.charAt(i) - 'a']++;
+            b_num[b.charAt(i) - 'a']++;
+        }
+        return Arrays.equals(a_num, b_num);
+    }
+    
+    public int getPairs(String s) {
+        int pairs = 0;
+        int len = s.length();
+        for(int li=1;li<len;li++) {
+            for(int i=0;i<len-li;i++) {
+                for(int j=i+1;j<len-li+1;j++) {
+                    if(isAnagram(s.substring(i,i+li), s.substring(j,j+li)))
+                        pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0;i<n;i++) {
+            Solution solution = new s20160405_3();
+            System.out.println(solution.getPairs(in.next()));
+        }
+    }
+}

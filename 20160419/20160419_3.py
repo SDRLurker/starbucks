cb2f8c06cadf984c6305671f02a144c9e7acbd4b
Ryan Shin <1982sdr@hanmail.net>
2020-01-25 23:09:34 +0900

    Starbucks Coding - 20160419_3 추가함. 모든 testcase 성공.

---
+++ 20160419/20160419_3.py
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def max_triangle(n):
+	max_product = -1
+	for a in range(3, n//3):
+		b = ( n * (n - 2*a) ) / ( 2 * (n - a) )
+		if ( n * (n - 2*a) ) % ( 2 * (n - a) ) == 0:
+			c = n - a - b
+			if a * b * c > max_product:
+				max_product = a * b * c
+	return max_product
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        self.assertEqual(max_triangle(12), 60)
+        self.assertEqual(max_triangle(4), -1)
+
+if __name__ == "__main__":
+    unittest.main()
+

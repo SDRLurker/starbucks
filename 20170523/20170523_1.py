ed88d2536a8a195dc55e9077b8b619c43b5a6979
Ryan Shin <1982sdr@hanmail.net>
2020-03-14 21:05:10 +0900

    Starbucks Coding - 20150523_1 추가함. 모든 testcase 성공.

---
+++ 20170523/20170523_1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        nr = len(nums)
+        nc = len(nums[0]) if nr > 0 else 0        
+        if nr*nc != r*c:
+            return nums
+        arr = [v for row in nums for v in row]
+        o = []
+        for y in range(r):
+            row_arr = []
+            for x in range(c):
+                row_arr.append(arr[y*c+x])
+            o.append(row_arr)
+        return o
+
+import unittest
+class test_solution(unittest.TestCase):
+    def test_all(self):
+        s = Solution()
+        i = [[1,2],[3,4]]
+        o = [[1,2,3,4]]
+        self.assertEqual(s.matrixReshape(i, 1, 4), o)
+        self.assertEqual(s.matrixReshape(i, 2, 4), i)
+        i = [[1,2,3,4]]
+        o = [[1,2],[3,4]]
+        self.assertEqual(s.matrixReshape(i, 2, 2), o)
+        i = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16],[17,18,19,20]]
+        o = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+        self.assertEqual(s.matrixReshape(i, 4, 5), o)
+
+if __name__ == "__main__":
+    unittest.main()
--- 20170523/README.md
+++ 20170523/README.md
@@ -5,3 +5,7 @@ https://leetcode.com/problems/reshape-the-matrix/
 20170523_2 
 5월 23일 두번째 문제입니다.
 https://leetcode.com/problems/longest-harmonious-subsequence/
+
+20170523_3 
+5월 23일 세번째 문제입니다.
+https://www.hackerrank.com/contests/projecteuler/challenges/euler023/problem
